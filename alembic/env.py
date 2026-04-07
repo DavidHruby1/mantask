cf290a68085ace8af3c1a3a3b143d1eaa4b0fbce
DavidHruby1 <hrubyd74@gmail.com>
2026-04-07 22:04:00 +0200

    update import path

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import engine_from_config, pool
 
 from backend.app.core.db import Base
 from backend.app.models.app_config import AppConfig
-from backend.app.models.session import Session
+from backend.app.models.auth_session import AuthSession
 from backend.app.models.task import Task
 from backend.app.models.team import Team
 from backend.app.models.team_member import TeamMember
