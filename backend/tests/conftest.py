969fbd38dd8a072aab9c30466c347f0d79d3514f
DavidHruby1 <hrubyd74@gmail.com>
2026-07-26 14:01:42 +0200

    chore: remove tests from movement PR

--- backend/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-# Unit and endpoint tests do not connect, but application imports require a valid URL.
-os.environ.setdefault(
-    "DATABASE_URL",
-    "postgresql+psycopg://postgres:postgres@localhost:55432/mantask_test",
-)
